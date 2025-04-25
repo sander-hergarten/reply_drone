e524a53c49b1bc6cb9ad00c4b2d3621f54b80ea4
sander-hergarten <mail@sander-hergarten.de>
2025-04-26 01:38:27 +0200

    changed datatype of other nodes to dict

--- APIs/engine_rl_agent.py
+++ APIs/engine_rl_agent.py
@@ -10,8 +10,8 @@ class SingleNodeState:
     rotation: tuple[float, float, float]
     depth_map: Image
     overlap_map: dict[int, float]
-    postion_of_other_nodes: list[tuple[float, float, float]]
-    rotation_of_other_nodes: list[tuple[float, float, float]]
+    postion_of_other_nodes: dict[int, tuple[float, float, float]]
+    rotation_of_other_nodes: dict[int, tuple[float, float, float]]
     id: int
 
 
