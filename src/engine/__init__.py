a2f8f9c64d41955f649130a7262e14018bc9076d
Eli L <elias@Elis-MacBook-Pro.local>
2025-04-25 22:47:25 +0200

    Added ply to gltf conversion and trimesh dependency

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,5 +1,10 @@
+import trimesh
+
 def main():
-    print("Hello World!")
+    mesh = trimesh.load("odm_mesh.ply")
+    print("Loaded ply")
+    mesh.export("odm_mesh.gltf")
+    print("Exported to gltf")
 
 if __name__ == "__main__":
     main()
